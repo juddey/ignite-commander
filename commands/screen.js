e03aff75a811e26eb43b382b6ccfb05b64b1a120
Justin Lane <justin@justinlane.org>
2018-12-23 18:32:58 +1300

    Update Screen Template

--- commands/screen.js
+++ commands/screen.js
@@ -1,15 +1,24 @@
-// @cliDescription  Generates a component, index and a storybook test.
+// @cliDescription  Generates a screen, and apollo mutation.
 
-const { path } = require('ramda')
+const { path, toUpper } = require('ramda')
 
 module.exports = async function (context) {
   // grab some features
-  const { parameters, strings, print, ignite, filesystem, patching, prompt } = context
-  const { pascalCase, isBlank } = strings
+  const {
+    parameters,
+    strings,
+    print,
+    ignite,
+    filesystem,
+    patching,
+    prompt
+  } = context
+  const { pascalCase, isBlank, snakeCase, upperCase, camelCase } = strings
   const options = parameters.options || {}
   const folder = options.folder || options.f
   const config = ignite.loadIgniteConfig()
-  const format = path(['ignite-base-plate', 'format'], config)  
+  const format = path(['ignite-base-plate', 'format'], config)
+  const jobs = []
 
   // validation
   if (isBlank(parameters.first)) {
@@ -19,19 +28,30 @@ module.exports = async function (context) {
   }
 
   const name = parameters.first
+  const queryName = isBlank(parameters.second)
+    ? parameters.first
+    : parameters.second
   const pascalName = pascalCase(name)
-
-  const props = { name, pascalName }
+  const camelQuery = camelCase(queryName)
+  const screamingQuery = toUpper(snakeCase(queryName))
+  const props = { name, pascalName, screamingQuery, camelQuery }
 
   if (format === 'function') {
-    let jobs = [
+    jobs.push(
       {
         template: 'screen.js.ejs',
-        target: folder ? `./src/Screens/${folder}/${name}.js` : `./src/Screens/${name}.js`
+        target: folder
+          ? `./src/Screens/${folder}/${name}.js`
+          : `./src/Screens/${name}.js`
+      },
+      {
+        template: 'mutation.js.ejs',
+        target: folder
+          ? `./src/Screens/${folder}/${name}.mutation.js`
+          : `./src/Screens/${name}.mutation.js`
       }
-    ]  
+    )
   }
 
-   await ignite.copyBatch(context, jobs, props)
-
+  await ignite.copyBatch(context, jobs, props)
 }
