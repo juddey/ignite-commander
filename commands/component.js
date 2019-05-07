5cf733791546e515cb28fa99a6523c3bef6b27b5
Justin Lane <justin@justinlane.org>
2019-05-08 09:01:02 +1200

    Remove Domains

--- commands/component.js
+++ commands/component.js
@@ -10,8 +10,8 @@ module.exports = async function (context) {
   const folder = options.folder || options.f
   const config = ignite.loadIgniteConfig()
   const storybooks = path(['ignite-base-plate', 'storybooks'], config)
-  const format = path(['ignite-base-plate', 'format'], config)  
-  const componentPath = path(['ignite-base-plate', 'componentpath'], config)    
+  const format = path(['ignite-base-plate', 'format'], config)
+  const componentPath = path(['ignite-base-plate', 'componentpath'], config)
 
   // validation
   if (isBlank(parameters.first)) {
@@ -19,77 +19,26 @@ module.exports = async function (context) {
     print.info('A name is required.')
     return
   }
-   
-  const domains = format === 'feature' ? filesystem.list('./src/views/') : []
-  const domainChoices = format === 'feature' ? ['(Create New)', ...domains] : []
-    
-  let domainAddAnswer = {}
-  let domainPath = ''
+
   let jobs = []
-  if (format === 'feature') {
-    if (!folder) {
-      const domainQuestion = 'Add component to which domain?'
-      domainAddAnswer = await prompt.ask({
-        name: 'domain',
-        type: 'list',
-        message: domainQuestion,
-        choices: domainChoices
-      })
-      domainPath = (domainAddAnswer.domain === domainChoices[0]) ? '' : domainAddAnswer.domain + '/'
-    } else {
-      domainPath = (folder === 'views') ? '' : folder + '/'
-    }
-  }
 
   const name = parameters.first
   const pascalName = pascalCase(name)
-
   const props = { name, pascalName }
-  if (format === 'feature') {
-    jobs.push(
-      {
-        template: 'component.js.ejs',
-        target: `src/views/${domainPath}${name}/${name}.js`
-      }, {
-        template: 'rollup-index.js.ejs',
-        target: `src/views/${domainPath}${name}/index.js`
-      }
-    )
-  
-    if (storybooks) {
-      jobs.push({
-        template: 'component.story.js.ejs',
-        target: `src/views/${domainPath}${name}/${name}.story.js`
-      })
-    }
-  }
 
-  if (format === 'function') {
-    jobs.push({
-        template: 'component.js.ejs',
-        target: folder ? `${componentPath}${folder}/${name}.js` : `${componentPath}${name}.js`
-    })   
-  
-    if (storybooks) {
-      jobs.push({
-        template: 'component.story.js.ejs',
-        target: folder ? `${componentPath}${folder}/${name}.story.js` : `${componentPath}${name}.story.js`
-      })
-    }
-  }
-
-   await ignite.copyBatch(context, jobs, props)
+  jobs.push({
+    template: 'component.js.ejs',
+    target: folder ? `${componentPath}${folder}/${name}.js` : `${componentPath}${name}.js`
+  })
 
   if (storybooks) {
-    // wire up example
-    if (format === 'feature') {
-      patching.insertInFile('./storybook/storybook-registry.js', '\n', `require('../src/views/${domainPath}${name}/${name}.story')`)
-    } else {
-        if (folder) { 
-          patching.insertInFile('./storybook/storybook-registry.js', '// StoryBook Registry', `import '~${tail(componentPath)}${folder}/${name}.story'`)
-      } else {
-          patching.insertInFile('./storybook/storybook-registry.js', '// StoryBook Registry', `import '~${tail(componentPath)}${name}.story'`)
-      }
-    }
+    jobs.push({
+      template: 'component.story.js.ejs',
+      target: folder
+        ? `${componentPath}${folder}/${name}.story.js`
+        : `${componentPath}${name}.story.js`
+    })
   }
+
+  await ignite.copyBatch(context, jobs, props)
 }
